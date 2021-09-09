4d75a62864de357234038dd4e5d759e7d9b0aef6
Fulliton <artyshko.andrey@gmail.com>
2021-09-09 19:02:09 +0700

    Product and filter

--- resource/js/app.js
+++ resource/js/app.js
@@ -1,4 +1,6 @@
 import 'bootstrap'
+import * as noUiSlider from 'nouislider';
+
 document.addEventListener('DOMContentLoaded', function(){
 
   // let inputSearch = document.getElementById('search')
@@ -37,5 +39,83 @@ document.addEventListener('DOMContentLoaded', function(){
     document.body.style.overflowY = 'auto'
 
   })
+
+  let filterOptions = document.getElementsByClassName('filter__options')
+
+  Array.from(filterOptions).forEach(filter => {
+    console.log(filter.dataset)
+    let uiMin = Number(filter.dataset.uiMin)
+    let uiMax = Number(filter.dataset.uiMax)
+    let range = filter.querySelector('.range')
+
+    let startInput = filter.querySelector('input.start__range')
+    let endInput = filter.querySelector('input.end__range')
+
+    console.log(startInput, endInput)
+
+    noUiSlider.create(range, {
+      range: {
+        'min': uiMin,
+        'max': uiMax
+      },
+      step: 1,
+      start: [uiMin, uiMax],
+      connect: true,
+      orientation: 'horizontal',
+    })
+
+    range.noUiSlider.on('update', ( values, handle ) => {
+      let value = Number(values[handle]);
+      if ( handle ) {
+        endInput.value= Number(value)
+      } else {
+        startInput.value = Number(value)
+      }
+    })
+
+    startInput.addEventListener('change', function () {
+      range.noUiSlider.set([this.value, null]);
+    });
+
+    endInput.addEventListener('change', function () {
+      range.noUiSlider.set([null, this.value]);
+    });
+
+  })
+
+  let sliders = document.getElementsByClassName('slider')
+
+  Array.from(sliders).forEach(function (slider) {
+    let sections = slider.querySelectorAll('.section');
+    let slides = slider.querySelectorAll('img');
+
+
+
+    sections.forEach(section => {
+      section.addEventListener('mouseenter', function (e) {
+
+        let lastIndicator = Array.from(sections).find(e => {
+          return e.querySelectorAll('.indicator')[0].classList.contains('indicator_full')
+        })
+        console.log(lastIndicator)
+
+        lastIndicator.querySelectorAll('.indicator')[0].classList.remove('indicator_full');
+        this.querySelectorAll('.indicator')[0].classList.add('indicator_full');
+        changeSlide(e.target, slides);
+      });
+    })
+  })
+
+
+  function changeSlide(section, slides) {
+    slides.forEach(slide => {
+      if (section.dataset.id === slide.dataset.id)
+        slide.classList.remove('opacity-0');
+      else
+        slide.classList.add('opacity-0');
+    })
+  }
+
+
 
 });
